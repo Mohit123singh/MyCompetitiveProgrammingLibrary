65bfb70c0551861c46b52dff6ebb2c7b60c7a89f
Mohit Singh <mohiit.singh600@gmail.com>
2022-04-04 19:00:55 +0530

    Added Sliding-Window-Technique

--- 05-Sliding-Window-Technique.cpp
+++ 05-Sliding-Window-Technique.cpp
@@ -1,4 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+
 // Sliding-Window-Technique::
+
+// Maximum-Sum in Window(K-size) in an Array
+
+int find_maximum_sum(vector<int> a, int k, int n)
+{
+    int sum = 0;
+
+    // find the sum of first k elements:
+
+    for (int i = 0; i < k; i++)
+    {
+        sum += a[i];
+    }
+    int max_value = sum;
+
+    // find-maximum-sum in every window-size of k :
+
+    for (int i = k; i < n; i++)
+    {
+        sum += a[i];
+        sum -= a[i - k];
+        max_value = max(max_value, sum);
+    }
+    return max_value;
+}
 
 // Problem1 : https://codeforces.com/problemset/problem/363/B
 // Solution : https://codeforces.com/contest/363/submission/146230482
