9cfd96dcbf5cfeba4a9ab9d3b131dc71b86b2a49
Mohit Singh <mohiit.singh600@gmail.com>
2022-04-04 19:30:42 +0530

    Added prefix-sum-2D-technique

--- 03-PrefixSum2D.cpp
+++ 03-PrefixSum2D.cpp
@@ -1,124 +1,33 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
 using namespace std;
-
-#define ff first
-#define ss second
-#define ll long long int
-#define int long long int
-#define type int
-#define type1 int
-#define type2 int
-#define pb push_back
-#define mp make_pair
-#define pii pair<type, type>
-#define vi vector<type>
-#define fo(i, n) for (type i = 0; i < n; i++)
-#define foo(i, n) for (type i = 1; i <= n; i++)
-#define mi(x) map<type1, type2> x
-#define mii(x) unordered_map<type1, type2> x
-#define sta stack<type>
-#define q1 queue<type>
-#define stt unordered_set<type>
-#define maxheap priority_queue<type>
-#define minheap priority_queue<type, vi, greater<type>>
-#define setbits(x) __builtin_popcountll(x)
-#define zrobits(x) __builtin_ctzll(x)
-#define mod 1000000007
-#define PI 3.14159265358979323846
-#define inf 1e18
-#define ps(x, y) fixed << setprecision(y) << x
-#define mk(arr, n, typee) typee *arr = new typee[n];
-#define test(x) \
-    int x;      \
-    cin >> x;   \
-    while (x--)
-//#define mt19937             rng(chrono::steady_clock::now().time_since_epoch().count());
-#define endl "\n"
-#define sz() size()
-#define ct continue
-#define br break
-#define rr return
-#define I(x) x.begin(), x.end()
-#define G(x) x.rbegin(), x.rend()
-#define acc accumulate
-
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
 
 // prefix-sum in 2d matrix:
 
-void solve()
+const int N = 100;
+
+int dp[N][N], matrix[N][N];
+
+void create_prefix2d_array(int n, int m)
 {
-    int n, m;
-    cin >> n >> m;
-
-    int matrix[n + 1][m + 1] = {0};
-
-    foo(i, n)
+    for (int i = 1; i <= n; i++)
     {
-        foo(j, m) cin >> matrix[i][j];
+        for (int j = 1; j <= m; j++)
+        {
+            dp[i][j] = matrix[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+        }
     }
-
-    int pre[n + 1][m + 1] = {0};
-
-    foo(i, n)
-    {
-        foo(j, m)
-            pre[i][j] = matrix[i][j] + pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
-    }
-    int q;
-    cin >> q;
-    foo(i, q)
-    {
-        int r1, c1, r2, c2;
-        cin >> r1 >> c1 >> r2 >> c2;
-
-        int sum = pre[r2][c2];
-        sum -= pre[r1 - 1][c2];
-        sum -= pre[r2][c1 - 1];
-        sum += pre[r1 - 1][c1 - 1];
-
-        cout << sum << endl;
-    }
-
-    rr;
 }
 
-/*
-Practice Problems:
-
- https://www.codechef.com/COW42020/problems/COW3E/
-
- https://codeforces.com/problemset/problem/835/C
-
-
-*/
-
-int32_t main()
+int get_sum_of_sub_rectange(int r1, int c1, int r2, int c2)
 {
+    int sum = dp[r2][c2];
+    sum -= dp[r1 - 1][c2];
+    sum -= dp[r2][c1 - 1];
+    sum += dp[r1 - 1][c1 - 1];
 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    //test(t)
-    solve();
-
-    return 0;
+    return sum;
 }
+
+// Problem 1 : https://codeforces.com/problemset/problem/835/C
+// Solution  : https://codeforces.com/contest/835/submission/146096181
