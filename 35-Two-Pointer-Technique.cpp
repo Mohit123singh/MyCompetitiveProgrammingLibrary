e7c0800dc40a596ccbc749d0252dfad0d43e7f9d
Mohit Singh <mohiit.singh600@gmail.com>
2022-04-04 18:45:02 +0530

    Added Two-Pointer-Technique

---
+++ 35-Two-Pointer-Technique.cpp
@@ -0,0 +1,8 @@
+// Problem 1 : https://codeforces.com/contest/701/problem/C
+// Solution : https://codeforces.com/contest/701/submission/152648040
+
+// Problem 2 : https://codeforces.com/contest/1352/problem/E
+// Solution : https://codeforces.com/contest/1352/submission/152656183
+
+// Problem 3 : https://codeforces.com/contest/1036/problem/D
+// Solution : https://codeforces.com/contest/1036/submission/152651020
