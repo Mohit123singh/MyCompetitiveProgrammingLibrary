9d9d8bc72b0a2414b0f83d22b821c8cd34a629ea
Mohit Singh <mohiit.singh600@gmail.com>
2022-04-05 19:05:02 +0530

    Added Bit-Manipulation-Concepts(with Problems)

---
+++ 36-Bit-Manipulation.cpp
@@ -0,0 +1,13 @@
+// Bit-Manipulation Concepts or Tricks:
+
+// Problem 1 : https://codeforces.com/contest/1420/problem/B
+// Solution  : https://codeforces.com/contest/1420/submission/152740057
+
+// Problem 2 : https://codeforces.com/contest/1362/problem/C
+// Solution  : https://codeforces.com/contest/1362/submission/152758028
+
+// Problem 3 : https://codeforces.com/problemset/problem/912/B
+// Solution  : https://codeforces.com/contest/912/submission/152762415
+
+// Problem 4 : https://codeforces.com/problemset/problem/1097/B
+// Solution  : https://codeforces.com/contest/1097/submission/152765731
