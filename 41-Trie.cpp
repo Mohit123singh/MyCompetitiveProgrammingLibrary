6e97becf01af39e49b7d89137983efad579f4eea
Lenovo <mohiit.singh600@gmail.com>
2022-06-08 16:12:04 +0530

    Added-Trie-Data-Structure

--- 41-Trie.cpp
+++ 41-Trie.cpp
@@ -2,77 +2,104 @@
 using namespace std;
 
 
-struct Node{
-	
+struct Node {
+
 	Node *links[2];
-	int cnt=0;
-	
+	int cnt = 0;
 	bool containsKey(int bit)
 	{
-		return links[bit]!=NULL;
+		return links[bit] != NULL;
 	}
-	void put(int bit,Node *node)
+	void put(int bit, Node *node)
 	{
-		links[bit]=node;
+		links[bit] = node;
 	}
-	
 	Node *get(int bit)
 	{
 		return links[bit];
 	}
+	void increaseCnt()
+	{
+		cnt++;
+	}
+	void decreseCnt()
+	{
+		cnt--;
+	}
+	int getCount(int bit)
+	{
+		return links[bit]->cnt;
+	}
+
 };
 
-class Trie{
-private:Node *root;
+class Trie {
+private: Node *root;
 public:
 	Trie()
 	{
-		root=new Node();
+		root = new Node();
 	}
-
 	void insert(int x)
 	{
-		Node *node=root;
-		for(int i=31;i>=0;i--)
+		Node *node = root;
+		for (int i = 31; i >= 0; i--)
 		{
-			int bit=((x>>i)&1);
-			if(!node->containsKey(bit))
-				node->put(bit,new Node());
-			node=node->get(bit);
-			node->cnt++;
+			int bit = (x >> i) & 1;
+			if (!node->containsKey(bit))
+				node->put(bit, new Node());
+			node = node->get(bit);
+			node->increaseCnt();
 		}
 	}
-	
-	int maxXor(int x)
+	int getMax(int x)
 	{
-		Node *node=root;
-		int ans=0;
-		for(int i=31;i>=0;i--)
+		Node *node = root;
+		int ans = 0;
+		for (int i = 31; i >= 0; i--)
 		{
-			int bit=((x>>i)&1);
-			if(node->containsKey(!bit))
+			int bit = (x >> i) & 1;
+			if (node->containsKey(!bit) and node->getCount(!bit))
 			{
-				ans|=(1<<i);
-				node=node->get(!bit);
+				ans |= (1 << i);
+				node = node->get(!bit);
 			}
 			else
-				node=node->get(bit);
+				node = node->get(bit);
+		}
+		return ans;
+	}
+
+	int getMin(int x)
+	{
+		Node *node = root;
+		int ans = 0;
+		for (int i = 31; i >= 0; i--)
+		{
+			int bit = (x >> i) & 1;
+			if (node->containsKey(bit) and node->getCount(bit))
+				node = node->get(bit);
+			else
+			{
+				ans |= (1 << i);
+				node = node->get(!bit);
+			}
 		}
 		return ans;
 	}
 
 	void remove(int x)
 	{
-		Node *node=root;
-		for(int i=31;i>=0;i--)
+		Node *node = root;
+		for (int i = 31; i >= 0; i--)
 		{
-			int bit=((x>>i)&1);
-			node=node->get(bit);
-			node->cnt--;
+			int bit = (x >> i) & 1;
+			node = node->get(bit);
+			node->decreseCnt();
 		}
 	}
-
 };
+
 
 // Problem1 : https://codeforces.com/contest/706/problem/D
 // Solution : https://codeforces.com/contest/706/submission/159881684
